081c776a0b6d30c6a05704af309adceea5d30e6f
eleni <eleni@yukisoftware.es>
2023-01-12 19:28:42 +0100

    Added user service. controller and fixed security

--- backend/src/main/java/com/ironhack/backend/service/UserService.java
+++ backend/src/main/java/com/ironhack/backend/service/UserService.java
@@ -1,5 +1,7 @@
 package com.ironhack.backend.service;
 
+import com.ironhack.backend.dto.UserDTO;
+import com.ironhack.backend.exceptions.UserNotFoundException;
 import com.ironhack.backend.model.User;
 import com.ironhack.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -35,5 +37,32 @@ public class UserService {
     public List<User> saveAll(List<User> listOfUsers) {
         return userRepository.saveAll(listOfUsers);
     }
+
+
+    public UserDTO createUser(UserDTO user) {
+        var userCreated = userRepository.save(User.fromDTO(user));
+        return UserDTO.fromUser(userCreated);
+    }
+
+    public User updateUser(Long id, User user) {
+        var userToUpdate = findUserById(id);
+        userToUpdate.setUsername(user.getUsername());
+        userToUpdate.setPassword(user.getPassword());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setRoles(user.getRoles());
+        return userRepository.save(userToUpdate);
+    }
+
+    public void deleteGame(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    private User findUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
 
 }
